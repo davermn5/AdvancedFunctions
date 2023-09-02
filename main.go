624c5b9131f631f4d129cc684025755d6dd7a801
JR <davermn5@gmail.com>
2023-09-02 09:24:01 -0700

    Updated documentation for pointer reference within the update() function.

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import "fmt"
 // a is a pointer to the same address (which resolves to the actual value 20) that the variable age already points to.
 // t is a pointer to the same address (which resolves to the actual value "John") that the variable text points to.
 func update(a *int, t *string) {
-	*a = *a + 5
-	*t = *t + " Doe"
+	*a = *a + 5      //Add 5 to the value which is being stored at the address that pointer 'a' and variable 'age' points to
+	*t = *t + " Doe" //Append " Doe" to the value which is being stored at the address that pointer 't' and variable 'text' points to
 	return
 }
 
